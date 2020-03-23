64fd24a9117cf514086b9520a3074ca37851ccb9
SilviaP46 <61684236+SilviaP46@users.noreply.github.com>
2020-03-23 09:49:27 +0200

    f2

--- Complex.cpp
+++ Complex.cpp
@@ -1,12 +1,59 @@
-#include "complex.h"
 #include <iostream>
 #include <cmath>
 #include <string>
+#include "Complex.h"
 using namespace std;
 
 Complex::Complex()
 {
 	r = 0;
 	i = 0;
+}
+
+Complex::Complex(double r, double i)
+{
+	this->r = r;
+	this->i = i;
+}
+
+
+double Complex::getterReal() const
+{	//return::the real part
+
+	return r;
+}
+
+
+void Complex::setterReal(double r) {
+	this->r = r;
+}
+
+
+double Complex::getterImag() const
+{	//return::the imaginary part
+
+	return i;
+}
+
+
+void Complex::setterImag(double im) {
+	this->i = i;
+}
+
+
+Complex Complex::addition(Complex other)
+{	//return::sum of 2 complex numbers
+
+	Complex Summe;
+	Summe.setterReal(this->getterReal() + other.getterReal());
+	Summe.setterImag(this->getterImag() + other.getterImag());
+	return Summe;
+}
+
+
+//Destructor
+Complex::~Complex()
+{
+
 }
 
--- Complex.h
+++ Complex.h
@@ -1,5 +1,4 @@
 #pragma once
-#pragma once
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -8,6 +7,22 @@ using namespace std;
 class Complex
 {
 private:
-	double re;
-	double im;
+	double r;
+	double i;
+
+public:
+	Complex(double r, double i);
+	Complex();
+
+	double getterReal() const;      //access method
+	void setterReal(double r);
+	double getterImag() const; //access method
+	void setterImag(double i);
+	Complex addition(Complex other);
+	~Complex();
+
+
+
+
+
 };
